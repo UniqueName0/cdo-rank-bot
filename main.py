b12af6592b2c96f765aa055ac7903dffb45d1d9a
UniqueName2 <name37655@gmail.com>
2020-12-08 04:35:37 +0000

    Z

---
+++ main.py
@@ -0,0 +1,36 @@
+import discord
+from discord.ext import commands
+import os
+import requests
+
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+import pytesseract
+
+token = os.getenv('token')
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
+        
+client = MyClient()
+        
+@client.event
+async def on_message(ctx):
+  attachment = ctx.attachments[0]
+  response = requests.get(attachment.url)
+
+  file = open("rankpic.png", "wb")
+  file.write(response.content)
+  file.close()
+  ranktext = pytesseract.image_to_string(Image.open("rankpic.png"))
+  print(ranktext)
+
+
+
+client.run(token)
