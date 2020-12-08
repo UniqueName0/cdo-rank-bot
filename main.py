f61a7b6b553bb299960f821c92ee0c5824655ad6
name37566 <75515676+name37566@users.noreply.github.com>
2020-12-08 13:45:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 import requests
 import cv2
 import numpy as np
+from colour import Color
 
 try:
     from PIL import Image
@@ -36,8 +37,12 @@ async def on_message(ctx):
   file.write(response.content)
   file.close()
 
+  z = Color(rgb = (255, 255, 0))
+  x = Color(rgb = (153, 102, 0))
+  c = list(x.range_to(z, 6))
+
   img = cv2.imread('rankpic.png')
-  img[img != [255, 255, 207]] = 255 # change everything to white where pixel is not black
+  img[img != c] = 255 # change everything to white where pixel is not black
   cv2.imwrite('rankpic-edited.png', img)
 
   ranktext = pytesseract.image_to_string(Image.open('rankpic-edited.png'),config='--psm 11')
