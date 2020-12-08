5899ea2e2520609dfc9b7abe6512c7e284337f5c
name37566 <75515676+name37566@users.noreply.github.com>
2020-12-08 12:21:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,8 @@ client = MyClient()
 async def on_message(ctx):
     if ctx.author == client.user:
         return
-      if ctx.author.bot: return
+      if ctx.author.bot:
+            return
     
   attachment = ctx.attachments[0]
   response = requests.get(attachment.url)
