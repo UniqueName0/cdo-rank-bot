5ebb5d97fa843f9681e34aeabad0d910eb7bf284
name37566 <75515676+name37566@users.noreply.github.com>
2020-12-08 21:12:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ async def on_message(ctx):
   c = list(x.range_to(z, 6))
 
   img = cv2.imread('rankpic.png')
-  img[img != '#FFCFA51D'] = 255 # change everything to white where pixel is not black
+  img[img != 255, 207, 165] = 255, 255, 255
   cv2.imwrite('rankpic-edited.png', img)
 
   ranktext = pytesseract.image_to_string(Image.open('rankpic-edited.png'),config='--psm 11')
