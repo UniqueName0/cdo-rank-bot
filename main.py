0eabc1221a63346deecf9fb8d9edbe63f961dc8b
name37566 <75515676+name37566@users.noreply.github.com>
2020-12-09 09:09:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -39,11 +39,11 @@ async def on_message(ctx):
   image = cv2.imread("rankpic.png")
   hsv=cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
 
-  yellow_lo=np.array([3, 157, 252])
-  yellow_hi=np.array([3, 252, 219])
+  yellow_lo=np.array([37, 0, 0])
+  yellow_hi=np.array([68, 255, 255])
   mask=cv2.inRange(hsv,yellow_lo,yellow_hi)
 
-  image[mask != 0]=(0,0,0)
+  image[mask>0]=(0,0,0)
 
   cv2.imwrite("rankpic-1.png",image)
   await ctx.channel.send(file=discord.File('rankpic-1.png'))
